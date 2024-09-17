55e35fa34e56343f154d1ab2e32d4ab4c4238f64
Leonard Jarlskog <ljarlskog@gmail.com>
2024-09-17 16:00:20 +0200

    Deleted stuff

--- bigint/src/main/scala/debugging_tools.scala
+++
@@ -1,8 +0,0 @@
-def print_bignum(big: BigNum) ={
-    print("[")
-    for (i <- 0 to big.value.length-1) {
-        print(s"${big.value(i)}")
-        if i < big.value.length-1 then print(", ")
-    }
-    print("]\n")
-}
